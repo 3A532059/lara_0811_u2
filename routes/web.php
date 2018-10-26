8a0d54adf3675f53ee0e078b545220bf8c30c927
3A532059 <coco214034@gmail.com>
2018-10-26 16:43:00 +0800

    練習5更正

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('say/{name?}', ['as' => 'hello.index', function($name ='Everybody') {
     return 'Hello, '.$name;
 }]);
 
+Route::get('dashboard', function () {
+    return 'dashboard';
+});
+
 Route::group(['prefix' => 'admin'], function() {
     Route::get('dashboard', function() {
         return 'admin dashboard';
